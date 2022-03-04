48f9a4a12a43a0792e50b8db6f11d6e417b3b492
MHRafix <fmhrafiz27@gmail.com>
2022-03-04 21:52:52 +0600

    code push

--- src/Pages/SharedComponents/BreadCrumb/BreadCrumb.js
+++ src/Pages/SharedComponents/BreadCrumb/BreadCrumb.js
@@ -1,34 +1,40 @@
-import { Breadcrumbs } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Breadcrumbs } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const BreadCrumb = ({breadcrumbNavigation}) => {
-    const { first, middle, last } = breadcrumbNavigation;
-    return (
-        <section style={{marginTop: '67px'}}>
-            <div className="breadCrumbArea">
-             <div className="breadcrumbNavigation">
-                 <Breadcrumbs aria-label="breadcrumb" sx={{textAlign: 'center', justifyContent: 'center', padding: {xs: '0px 10px', md: '0px'}}}>
-                  <Link to={`/${first.toLowerCase()}`} style={{ color: '#555', textDecoration: 'none', fontFamily: 'Poppins', fontWeight: 400, fontSize: 14 }} >
-                   {first?.toUpperCase()}
-                  </Link>
-                  {middle && <span
-                    style={{ color: '#555', textDecoration: 'none', fontFamily: 'Poppins', fontWeight: 400, fontSize: 14 }}
-                    >
-                    {middle?.toUpperCase()}
-                    </span>}
-                    <span
-                    disabled={true}
-                    aria-current="page"
-                    style={{ color: '#000', textDecoration: 'none', fontFamily: 'Poppins', fontWeight: 400, fontSize: 14 }}
-                    >
-                    {last?.toUpperCase()}
-                    </span>
-                </Breadcrumbs>
-             </div>
-            </div>
-        </section>
-    );
+const BreadCrumb = ({ breadcrumbNavigation }) => {
+  const { first, middle, last } = breadcrumbNavigation;
+  return (
+    <section className="breadCrumbContainer">
+      <div className="breadCrumbArea">
+        <div className="breadcrumbNavigation">
+          <Breadcrumbs
+            aria-label="breadcrumb"
+            sx={{
+              textAlign: "center",
+              justifyContent: "center",
+              padding: { xs: "0px 10px", md: "0px" },
+            }}
+          >
+            <Link to={`/${first.toLowerCase()}`} className="breadCrumbLink">
+              {first?.toUpperCase()}
+            </Link>
+            {middle && (
+              <span className="breadCrumbLink">{middle?.toUpperCase()}</span>
+            )}
+            <span
+              disabled={true}
+              aria-current="page"
+              className="breadCrumbLink"
+              style={{ color: "#222" }}
+            >
+              {last?.toUpperCase()}
+            </span>
+          </Breadcrumbs>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BreadCrumb;
